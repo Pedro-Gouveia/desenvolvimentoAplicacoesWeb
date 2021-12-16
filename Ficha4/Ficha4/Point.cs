e61aa5b8ee9434480d7fc48759300d97f55d6a1d
Pedro Gouveia <58569642+pedrogouveia69@users.noreply.github.com>
2021-12-16 11:24:42 +0000

    ex4

---
+++ Ficha4/Ficha4/Point.cs
@@ -0,0 +1,50 @@
+﻿namespace Ficha4
+{
+    internal class Point
+    {
+        private double x;
+        private double y;
+
+        public Point()
+        {
+            this.x = 0;
+            this.y = 0;
+        }
+
+        public Point(double x, double y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public double getX()
+        {
+            return x;
+        }
+
+        public double getY()
+        { 
+            return y;
+        }
+
+        public void setX(double x)
+        {
+            this.x = x;
+        }
+
+        public void setY(double y)
+        {
+            this.y = y;  
+        }
+
+        public void setXandY(double x, double y) { 
+            setX(x);
+            setY(y);
+        }
+
+        public double getDistanceTo(Point other) 
+        {
+            return Math.Sqrt(Math.Pow((other.x - this.x), 2) + Math.Pow((other.y - this.y), 2));
+        }
+    }
+}
---
+++ Ficha4/Ficha4/Program.cs
@@ -0,0 +1,9 @@
+﻿using Ficha4;
+
+var p1 = new Point(5, 10);
+var p2 = new Point(1, 6);
+var p3 = new Point(2, 3);
+
+Console.WriteLine(p1.getDistanceTo(p2));
+
+var t1 = new Triangle(p1, p2, p3);
